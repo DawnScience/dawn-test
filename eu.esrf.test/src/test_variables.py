7494aa949256067c4e069c952b0ddd185f96a8cb
Jonathan Pommier <pommier@ub1004.esrf.fr>
2013-09-19 13:59:28 +0200

    last script change

--- eu.esrf.test/src/test_variables.py
+++ eu.esrf.test/src/test_variables.py
@@ -1,22 +1,26 @@
-#!/usr/bin/python2.6
-import os,shutil,subprocess
-queue = {'tst_suite': 'suite_workflows_general','tst_case':'tst_loop_example'}
-
-print '/scisoft/jenkins/ub1004_jonathan/workspace/Dawn_squish_tests/org.dawnsci.squishtests/'+queue["tst_suite"]+'/shared/'
-def check_variable():
-    path_global_script='/scisoft/jenkins/ub1004_jonathan/workspace/Dawn_squish_tests/org.dawnsci.squishtests/'+queue["tst_suite"]+'/shared/scripts'
-    if os.path.exists(path_global_script)== True:
-        shutil.rmtree(path_global_script)         
-    shutil.copytree('/scisoft/jenkins/ub1004_jonathan/workspace/Dawn_squish_tests/eu.esrf.test/global_script_esrf',path_global_script)
-        
-        
-check_variable()
-
-os.system('sh script_jenkins.sh')
-
-#if test -z "$1" 
-#then
-
-#else 
-    
- #   fi
+#!/usr/bin/python2.6 
+import os,shutil 
+#only one queue allowed for one suite_test_case 
+queue = {'suite_workflows_general': {'tst_case2':'tst_open_examples'}} 
+#queue.update({'suite_usecases_general':{'tst_case':'tst_hdf5_large_tree'}}) 
+queue.update({'suite_workflows_bignexus':{'tst_case':'tst_slicedata_dbrowsing'}}) 
+#queue['suite_workflows_bignexus'] = {'tst_case':'tst_slicedata_dexplore'} 
+       
+def check_variable(testSuiteName): 
+    path_global_script=os.path.join(os.environ['WORKSPACE'],'org.dawnsci.squishtests',testSuiteName,'shared/scripts') 
+    if os.path.exists(path_global_script)== True: 
+        shutil.rmtree(path_global_script)          
+    shutil.copytree('/scisoft/jenkins/ub1004_jonathan/workspace/Dawn_squish_tests/eu.esrf.test/global_script_esrf',path_global_script) 
+            
+def setup_log(): 
+    path_log=os.path.join(os.environ['WORKSPACE']  +'/log/') 
+    if os.path.exists(path_log)== True: 
+        shutil.rmtree(path_log) 
+    os.mkdir(path_log) 
+ 
+setup_log() 
+for testSuiteName in queue.keys(): 
+    check_variable(testSuiteName) 
+    for tst in queue[testSuiteName]: 
+        os.chdir(os.path.join(os.environ['WORKSPACE'],'eu.esrf.test/src')) 
+        os.popen("./script_jenkins "+testSuiteName+" "+queue[testSuiteName][tst], "r").read()
