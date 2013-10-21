83e3e00104ec25d796e0735758abec991da24884
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2013-10-21 11:26:01 +0100

    Make box profile squish test work with profile submenus

--- org.dawnsci.squishtests/suite_tools2d_boxprofile/tst_basic/test.py
+++ org.dawnsci.squishtests/suite_tools2d_boxprofile/tst_basic/test.py
@@ -22,7 +22,8 @@ def main():
     
 
     mouseClick(waitForObject(":Image tools used to profile and inspect images._ToolItem"), vals.TOOL_X, vals.TOOL_Y, 0, Button.Button1)
-    activateItem(waitForObjectItem(":Pop Up Menu", "Box Profile"))
+    activateItem(waitForObjectItem(":Pop Up Menu", "Profile"))
+    activateItem(waitForObjectItem(":Profile_Menu", "Box Profile"))
     
     c = waitForObject(":Image_Composite")
     b = c.bounds
