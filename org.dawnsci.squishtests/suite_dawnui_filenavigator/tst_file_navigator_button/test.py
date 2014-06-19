ae148cf20fd87ee91b57994718e627f43d1927b6
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2014-06-19 17:26:01 +0100

    Add File navigator squish test on toolbar buttons

---
+++ org.dawnsci.squishtests/suite_dawnui_filenavigator/tst_file_navigator_button/test.py
@@ -0,0 +1,29 @@
+source(findFile("scripts", "dawn_global_startup.py"))
+source(findFile("scripts", "dawn_global_plot_tests.py"))
+
+# UI test to check that an hdf5 file can be opened and its tree can be expanded 
+def main():
+    # Start or attach runs (or attaches) to DAWN and then 
+    # makes sure the workbench window exists and finally
+    # will close the Welcome screen 
+    startOrAttachToDAWN()
+
+    clickTab(waitForObject(":File Navigator_CTabItem"), 12, 8, 0, Button.Button1)
+
+    mouseClick(waitForObject(":Refresh file tree_ToolItem"), 11, 13, 0, Button.Button1)
+    test.passes("File Navigator refresh button: Success")
+    
+    mouseClick(waitForObject(":Collapse All_ToolItem"), 14, 14, 0, Button.Button1)
+    test.passes("File Navigator collapse button: Success")
+
+    mouseClick(waitForObject(":Refresh file tree_ToolItem"), 15, 11, 0, Button.Button1)
+    test.passes("File Navigator refresh button: Success")
+    
+    mouseClick(waitForObject(":Alphanumeric sort for everything._ToolItem"), 13, 9, 0, Button.Button1)
+    test.passes("File Navigator sort everything button: Success")
+
+    mouseClick(waitForObject(":Sort alphanumeric, directories at top._ToolItem"), 8, 13, 0, Button.Button1)
+    test.passes("File Navigator sort folder on top button: Success")
+    
+     # Exit (or disconnect) DAWN
+    closeOrDetachFromDAWN()
