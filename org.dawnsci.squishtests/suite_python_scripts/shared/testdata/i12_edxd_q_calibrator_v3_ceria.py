0908e65a100fe6fbbf557fd01a46febfe5a7675c
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2014-09-11 16:16:51 +0100

    Commit squish test for i12 python script (auto peak fitting)

---
+++ org.dawnsci.squishtests/suite_python_scripts/shared/testdata/i12_edxd_q_calibrator_v3_ceria.py
@@ -0,0 +1,153 @@
+'''
+Created on 14 Sep 2011
+Revised 10 Oct 2012 by Thomas Connolley
+
+@author: ssg37927
+'''
+
+#
+# JYTHON Script
+#
+
+import scisoftpy as dnp
+from uk.ac.diamond.scisoft.analysis.plotserver import GuiParameters as _guiparam
+
+
+class EDXDCalibrator(object):
+    '''
+    classdocs
+    '''
+    
+    spectrun_window = "Plot 1"
+    calibration_window = "Plot 2"
+    ## Comment or uncomment these lines to select the calibration material you wish to use
+    ## Silicon
+    ##standard_peaks = [2.0036, 3.2719, 3.8366, 4.6271, 5.0423, 5.6671, 6.0108, 6.5438, 6.8436]
+    ## Lab6 (check before using)
+    ##standard_peaks = [1.5115, 2.1376, 2.6180, 3.0230, 3.3798, 3.7024, 4.2752, 4.5345, 4.7798, 5.0131, 5.2360, 5.4498, 5.6555, 6.0460, 6.2321, 6.4128, 6.5885, 6.9266, 7.0896, 7.4048, 7.5575, 7.7072, 7.8540]
+    ##CeO2(764b)
+    standard_peaks = [2.010944, 2.322096, 3.28394, 3.850761, 4.021988, 4.644192, 5.060891, 5.192364, 5.68795, 6.032982, 6.567879, 6.868852, 6.966288]
+    ##
+    #standard_peaks = []
+
+
+    def __init__(self, filename):
+        '''
+        Constructor
+        '''
+        self.data = dnp.io.load(filename)
+        self.results = {}
+        
+    
+    def load_data_element(self, element_number):
+        identifier = "/entry1/EDXD_Element_%02i/data" % element_number
+        element = self.data[identifier][:]
+        #energy = self.data.getLazyDataset("/entry1/EDXD_Element_%02i/edxd_energy_approx" % element_number).getSlice(None,None)
+        counts = element.sum(0)
+        energy = dnp.arange(counts.shape[0])
+        return (counts, energy)
+    
+    def plot_element(self, element_number):
+        (element, energy) = self.load_data_element(element_number)
+        dnp.plot.updateline(energy, element,self.spectrun_window)
+    
+    def get_peak_list_from_view(self):
+        fp = _guiparam.FITTEDPEAKS
+        bean = dnp.plot.getbean(self.spectrun_window)
+        print bean
+        peaks = bean[fp]
+        return peaks
+    
+    def calc_bins_vrs_q(self):
+        peaklist = self.get_peak_list_from_view()
+        peakpos = []
+        for peak in peaklist :
+            peakpos.append(peak.getPosition())
+        peakpos.sort()
+        x = dnp.array(self.standard_peaks)
+        y = dnp.array(peakpos)
+        max_size = min(x.shape[0], y.shape[0])
+        x = x[0:max_size]
+        y = y[0:max_size]
+        x.name = "Calibration Values"
+        y.name = "Peak positions"
+        fr = dnp.fit.polyfit(y, x, 2, full=True)
+        return (x,y,fr[1])
+    
+    def calibrate_elements(self, start, stop):
+        for i in range(start, stop):
+            self.plot_element(i)
+            print "Select Peaks in Plot 1 for element %i" % i
+            void = raw_input("Press enter here when finished selecting peaks")
+            (x,y,fr) = calib.calc_bins_vrs_q()
+            self.results[i] = (x,y,fr)
+        print "********************"
+        print "Calibration Complete"
+        print "********************"
+    
+    def report(self):
+        print "==========================="
+        print "Calibration Function report"
+        print "==========================="
+        keys = self.results.keys()
+        keys.sort()
+        for key in keys:
+            result = self.results[key][2]
+            print "Element %i, has f(x) = %f*x^2 + %f*x + %f with residual %f" % (key, result[0], result[1], result[2], result.residual )
+    
+    def full_report(self):
+        print "============================="
+        print "Calibration Per Peak Analysis"
+        print "============================="
+        keys = self.results.keys()
+        keys.sort()
+        string = "Element  "
+        for peak in self.standard_peaks:
+            string += "%1.4f  " % peak
+        string += "|  Residual  "
+        print string
+        length = len(string)
+        string = ""
+        for i in range(length):
+            string += "-"
+        print string
+        for key in keys:
+            result = self.results[key][2]
+            string = "  %02i     " % key
+            i = 0;
+            for point in self.results[key][1] :
+                value = result[0]*point*point + result[1]*point + result[2]
+                string += "%1.4f  " % abs(self.standard_peaks[i] - value)
+                i += 1
+            string += "|  %1.6f" % result.residual
+            print string
+    
+    def display_parameters(self, parameter_number):
+        keys = self.results.keys()
+        keys.sort()
+        data = []
+        for key in keys:
+            result = self.results[key][2]
+            data.append(result[parameter_number])
+        y = dnp.array(data)
+        x = dnp.array(keys)
+        dnp.plot.line(x,y,name=self.calibration_window);
+    
+    def print_report(self, filename):
+        file = open(filename, 'w')
+        keys = self.results.keys()
+        keys.sort()
+        for key in keys:
+            result = self.results[key][2]
+            file.write("%02i, %1.10e, %1.10e, %1.10e\n" % (key, result[0], result[1], result[2]))
+        file.close()
+        
+if __name__ == "__main__" :
+    print "Calibrating EDXD Q axis"
+    calib = EDXDCalibrator("/scratch/workspace/suite_python_scripts/tst_i12_auto_peakfitting/workspace/data/examples/i12/36153.nxs")
+    calib.calibrate_elements(1, 24)
+    calib.report()
+    calib.full_report()
+    calib.print_report("/scratch/workspace/suite_python_scripts/tst_i12_auto_peakfitting/workspace/data/examples/i12/36153_TESTING_2014-04-23_calib_CeO2.txt")
+    # finally display the curve shown by the second parameter(could be useful)   
+    calib.display_parameters(1)
---
+++ org.dawnsci.squishtests/suite_python_scripts/tst_i12_auto_peakfitting/test.py
@@ -0,0 +1,114 @@
+source(findFile("scripts", "dawn_global_startup.py"))
+source(findFile("scripts", "dawn_global_plot_tests.py"))
+source(findFile("scripts", "use_case_utils.py"))
+source(findFile("scripts", "file_utils.py"))
+source(findFile("scripts", "dawn_constants.py"))
+source(findFile("scripts", "dawn_global_python_setup.py"))
+
+import platform
+import os.path
+
+# UI test to check that an hdf5 file can be opened and its tree can be expanded 
+def main():
+    # Start or attach runs (or attaches) to DAWN and then 
+    # makes sure the workbench window exists and finally
+    # will close the Welcome screen 
+    startOrAttachToDAWN()
+
+    setupEPDPython()
+    #create i12 folder in data/examples
+    createDirectory("/scratch/workspace/suite_python_scripts/tst_i12_auto_peakfitting/workspace/data/examples", "i12")
+    #Add peema test files to data project
+    addExternalFile("36153.nxs", "suite_python_scripts", "tst_i12_auto_peakfitting", "data", "examples/i12")
+    addExternalFile("i12_edxd_q_calibrator_v3_ceria.py", "suite_python_scripts", "tst_i12_auto_peakfitting", "data", "examples/i12")
+    #Open Python perspective
+    openPerspective("Python") 
+    
+    snooze(15)
+    mouseClick(waitForObject(":Open Console_ToolItem"), 33, 11, 0, Button.Button1)
+    activateItem(waitForObjectItem(":Pop Up Menu", "5 PyDev Console"))
+    clickButton(waitForObject(":Jython console_Button"))
+    clickButton(waitForObject(":OK_Button"))
+    snooze(15)
+     #expand data tree and open metal mix
+    expand(waitForObjectItem(":PyDev Package Explorer_Tree", "data"))
+    expand(waitForObjectItem(":PyDev Package Explorer_Tree", "examples"))
+    expand(waitForObjectItem(":PyDev Package Explorer_Tree", "i12"))
+
+    children = object.children(waitForObjectItem(":PyDev Package Explorer_Tree", "i12"))
+    
+    for child in children:
+        if "i12_edxd_q_calibrator_v3_ceria.py" in child.text:
+            doubleClick(child, 5, 5, 0, Button.Button1)
+            continue
+    
+    clickTab(waitForObject(":i12_edxd_q_calibrator_v3_ceria_CTabItem"), 131, 15, 0, Button.Button1)
+    mouseClick(waitForObject(":Activates the interactive console. (Ctrl+Alt+Enter)_ToolItem"), 12, 8, 0, Button.Button1)
+    snooze(2)
+    clickTab(waitForObject(":i12_edxd_q_calibrator_v3_ceria_CTabItem"), 131, 15, 0, Button.Button1)
+    mouseClick(waitForObject(":Activates the interactive console. (Ctrl+Alt+Enter)_ToolItem"), 12, 8, 0, Button.Button1)
+    snooze(2)
+    
+    #change y-axis to log
+    mouseClick(waitForObject(":Configure Settings..._ToolItem_4"), 7, 10, 0, Button.Button1)
+    clickTab(waitForObject(":Configure Graph Settings.Axes_TabItem"))
+    mouseClick(waitForObjectItem(":Select Axis_Combo", "Y-Axis(Y-Axis)"), 0, 0, 0, Button.NoButton)
+    clickButton(waitForObject(":Change Settings.Log_Button"))
+    clickButton(waitForObject(":Configure Graph Settings.OK_Button"))
+    
+    snooze(1)
+    
+    system = getPlottingSystem("Plot 1")
+
+    test.verify(system.getTraces().iterator().next().getData().getRank()==1, "Data plotted: Success")
+#    proxy = waitForObject(":Plot 1_CTabItem")
+#    c = proxy.control
+#    b = c.bounds
+    c = system.getPlotComposite()
+    b = c.bounds
+    test.log("Image at (%d, %d) is %d x %d" % (b.x,b.y, b.width, b.height))
+
+    clickTab(waitForObject(":Plot 1_CTabItem"), 25, 11, 0, Button.Button1)
+
+    mouseClick(waitForObject(":XY plotting tools_ToolItem_3"), 32, 10, 0, Button.Button1)
+    activateItem(waitForObjectItem(":Pop Up Menu", "Maths and Fitting"))
+    activateItem(waitForObjectItem(":Maths and Fitting_Menu", "Peak Fitting"))
+
+    clickTab(waitForObject(":Peak Fitting_CTabItem"), 45, 8, 0, Button.Button1)
+    mouseClick(waitForObject(":Number peaks to fit_ToolItem_2"), 33, 8, 0, Button.Button1)
+    activateItem(waitForObjectItem(":Pop Up Menu", "Fit 7 Peaks"))
+    clickTab(waitForObject(":Plot 1_CTabItem"), 30, 10, 0, Button.Button1)
+
+    mouseClick(waitForObject(":XY plotting tools_ToolItem_3"), 18, 10, 0, Button.Button1)
+    sx1,sy1 = getScreenPosition(system,408,1500)
+    ex1,ey1 = getScreenPosition(system,3130,1500)
+    snooze(0.5)
+    mouseDrag(c, sx1, sy1, ex1, ey1, 0, Button.Button1)
+    snooze(2)
+
+    mouseClick(waitForObject(":PyDev Console"), 436, 255, 0, Button.Button1)
+    
+    for x in range(0, 24):
+        type(waitForObject(":PyDev Console"), "<Return>")
+        snooze(3)
+
+    #open plot view result    
+    activateItem(waitForObjectItem(":_Menu", "Window"))
+    activateItem(waitForObjectItem(":Window_Menu", "Show Plot View"))
+    activateItem(waitForObjectItem(":Show Plot View_Menu", "Plot 2 *"))
+    system = getPlottingSystem("Plot 2")
+    test.verify(system.getTraces().iterator().next().getData().getRank()==1, "Data plotted: Success")
+    
+    #open result file
+    mouseClick(waitForObjectItem(":PyDev Package Explorer_Tree", "data"), 11, 11, 0, Button.Button1)
+    type(waitForObject(":PyDev Package Explorer_Tree"), "<F5>")
+    children = object.children(waitForObjectItem(":PyDev Package Explorer_Tree", "i12"))
+    
+    for child in children:
+        if "36153__TESTING__2014-04-23__calib__CeO2.txt" in child.text:
+            doubleClick(child, 5, 5, 0, Button.Button1)
+            continue
+
+    test.passes("Result file successfully created")
+
+    closeOrDetachFromDAWN()
