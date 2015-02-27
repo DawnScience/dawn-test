3ea57506f66d569755973b7426264b5b63c51763
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2015-02-27 09:56:39 +0000

    Fix squish test with correct object map

--- org.dawnsci.squishtests/suite_dawnui_filenavigator/tst_file_navigator_button/test.py
+++ org.dawnsci.squishtests/suite_dawnui_filenavigator/tst_file_navigator_button/test.py
@@ -10,19 +10,19 @@ def main():
 
     clickTab(waitForObject(":File Navigator_CTabItem"), 12, 8, 0, Button.Button1)
 
-    mouseClick(waitForObject(":Refresh file tree_ToolItem"), 11, 13, 0, Button.Button1)
+    mouseClick(waitForObject(":Refresh file tree_ToolItem_2"), 11, 13, 0, Button.Button1)
     test.passes("File Navigator refresh button: Success")
     
-    mouseClick(waitForObject(":Collapse All_ToolItem"), 14, 14, 0, Button.Button1)
+    mouseClick(waitForObject(":Collapse All_ToolItem_2"), 14, 14, 0, Button.Button1)
     test.passes("File Navigator collapse button: Success")
 
-    mouseClick(waitForObject(":Refresh file tree_ToolItem"), 15, 11, 0, Button.Button1)
+    mouseClick(waitForObject(":Refresh file tree_ToolItem_2"), 15, 11, 0, Button.Button1)
     test.passes("File Navigator refresh button: Success")
     
-    mouseClick(waitForObject(":Alphanumeric sort for everything._ToolItem"), 13, 9, 0, Button.Button1)
+    mouseClick(waitForObject(":Alphanumeric sort for everything._ToolItem_2"), 13, 9, 0, Button.Button1)
     test.passes("File Navigator sort everything button: Success")
 
-    mouseClick(waitForObject(":Sort alphanumeric, directories at top._ToolItem"), 8, 13, 0, Button.Button1)
+    mouseClick(waitForObject(":Sort alphanumeric, directories at top._ToolItem_2"), 8, 13, 0, Button.Button1)
     test.passes("File Navigator sort folder on top button: Success")
     
      # Exit (or disconnect) DAWN
