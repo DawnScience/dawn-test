0cf1f70447d84e44a1726eba32d0e2452ff6a455
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-03-04 18:16:32 +0000

    Removes comments.

--- org.dawnsci.squishtests/suite_tools1d_peakfitting/tst_peak_multiple_peaks/test.py
+++ org.dawnsci.squishtests/suite_tools1d_peakfitting/tst_peak_multiple_peaks/test.py
@@ -21,29 +21,5 @@ def main():
     snooze(1)
     
     fitOneThenFourPeaks()
-#    mouseClick(waitForObject(":XY plotting tools_ToolItem_2"), 31, 7, 0, Button.Button1)
-#    activateItem(waitForObjectItem(":Pop Up Menu", "Maths and Fitting"))
-#    activateItem(waitForObjectItem(":Maths and Fitting_Menu", "Peak Fitting"))
-#    
-#    c = waitForObject(":Plot_Composite_2")
-#    b = c.bounds
-#
-#    test.log("Image at (%d, %d) is %d x %d" % (b.x,b.y, b.width, b.height))
-#    mouseDrag(c, b.x+b.width/3, b.y+b.height/3, int(b.width/1.7),b.height/3, 0, Button.Button1)
-#    snooze(5)
-#    
-#    tab = waitForObject(":Peak Fitting_Table")
-#    test.verify(tab.getItemCount()==1,"Expected: 1 Actual: " + str(tab.getItemCount()))
-#    
-#    test.verify(waitForObjectItem(":Peak Fitting_Table", "0/1").text == "Peak 1","peak 1 present")
-#    
-#    mouseClick(waitForObject(":Number peaks to fit_ToolItem"), 29, 10, 0, Button.Button1)
-#    activateItem(waitForObjectItem(":Pop Up Menu", "Fit 4 Peaks"))
-#    
-#    test.verify(tab.getItemCount()==4,"Expected: 4 Actual: " + str(tab.getItemCount()))
-#    
-#    for i in range(4):
-#        txt = waitForObjectItem(":Peak Fitting_Table",  str(i) + "/1").text
-#        test.verify(txt == "Peak " + str(i+1),"peak present")
-    
+        
     closeOrDetachFromDAWN()
