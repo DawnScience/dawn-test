b264b8d521ec366334e186ec238c04b17874a3ae
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-03-09 16:33:43 +0000

    New library script for interacting with common UI elements. Provides method to move sashes in any direction.

---
+++ org.dawnsci.squishtests/global_scripts/dawn_ui_controls.py
@@ -0,0 +1,41 @@
+source(findFile("scripts", "swt_ctabitems.py"))
+source(findFile("scripts", "swt_sashitems.py"))
+
+def createToolSpace(viewTabName=None, direction=None, steps=15):
+    if viewTabName is None:
+        raise LookupError("ERROR: Must specify a viewTabName to right click on!")
+    if direction is None:
+        raise LookupError("ERROR: Must specify a direction to drag the ")
+    
+    #To make test case insensitive
+    direction = direction.lower()
+    if direction == "up":
+        sashDirection = "Top"
+        arrowKey = "<Up>"
+    elif direction == "down":
+        sashDirection = "Bottom"
+        arrowKey = "<Down>"
+    elif direction == "left":
+        sashDirection = "Left"
+        arrowKey = "<Left>"
+    elif direction == "right":
+        sashDirection = "Right"
+        arrowKey = "<Right>"
+    else:
+    	raise LookupError("ERROR: Unrecognised direction to move sash (must be one of up, down, left or right (case insensitive).")
+    
+    viewTabObject = waitForFirstSwtCTabItem(item_text=viewTabName)
+    clickTab(waitForObject(viewTabObject), 49, 4, 0, Button.Button3)
+    activateItem(waitForObjectItem(":Pop Up Menu", "Size"))
+    activateItem(waitForObjectItem(":Size_Menu", "Top"))
+    
+    #Need to look at the properties of this object and work out (when this is active) what is the different property
+    #This will make it possible to select the right sash.
+    activeSash = waitForFirstSwtSashItem()
+    
+    #For compactness, put dragging sash into loop
+    a = 0
+    while a < steps:
+        type(activeSash, arrowKey)
+        a += 1
+    clickTab(waitForObject(viewTabObject), 27, 12, 0, Button.Button1)
