eacfc99858d1d2bfab8057dc72cdcddbfaccf38e
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2015-03-31 13:49:24 +0100

    Adds group with label finding method.

--- org.dawnsci.squishtests/global_scripts/swt_widgets.py
+++ org.dawnsci.squishtests/global_scripts/swt_widgets.py
@@ -35,3 +35,12 @@ def waitForSwtTextWithLabel(labelText, timeoutMSec=20000):
             i += 1
     
     raise LookupError('ERROR: Could not find SWT.Text with label "' + str(labelText) + '"')
+
+def waitForSwtGroupWithLabel(labelText):
+    
+    '''Tool to get the group container object.
+       WARNING: This will probably not work on squish-5.1 or higher!
+       '''
+    groupObj = waitForObject("{caption='"+labelText+"' parent.visible='true' type='org.eclipse.swt.widgets.Group'}")
+    snooze(2)
+    return groupObj
