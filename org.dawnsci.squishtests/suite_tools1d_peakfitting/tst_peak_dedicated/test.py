4ceee33fa7ecf54195a67a35029e6401dcde510c
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2015-12-22 15:19:49 +0000

    Fix peak fitting for e4

--- org.dawnsci.squishtests/suite_tools1d_peakfitting/tst_peak_dedicated/test.py
+++ org.dawnsci.squishtests/suite_tools1d_peakfitting/tst_peak_dedicated/test.py
@@ -1,6 +1,7 @@
 source(findFile("scripts", "dawn_global_startup.py"))
 source(findFile("scripts", "dawn_global_plot_tests.py"))
 source(findFile("scripts", "peakfit_shared.py"))
+source(findFile("scripts", "dawn_global_ui_controls.py"))
 
 def main():
     #Start using clean workspace
@@ -10,13 +11,17 @@ def main():
     openPerspective("Data Browsing (default)")
     
     #Create space so the drop-down menus show all items
-    createPeakFitSpace()
+    if(not isEclipse4()):
+        createPeakFitSpace()
     snooze(1)
     
     #expand data tree and open metal mix
     loadMetalMix()
     
-    mouseClick(waitForObjectItem(":Data_Table", "0/0"), 9, 7, 0, Button.Button1)
+    if(isEclipse4()):
+        mouseClick(waitForObjectItem(":Data_Table_2", "0/0"), 9, 7, 0, Button.Button1)
+    else:
+        mouseClick(waitForObjectItem(":Data_Table", "0/0"), 9, 7, 0, Button.Button1)
     
     snooze(1)
     
--- org.dawnsci.squishtests/suite_tools1d_peakfitting/tst_peak_multiple_traces/test.py
+++ org.dawnsci.squishtests/suite_tools1d_peakfitting/tst_peak_multiple_traces/test.py
@@ -1,6 +1,7 @@
 source(findFile("scripts", "dawn_global_startup.py"))
 source(findFile("scripts", "dawn_global_plot_tests.py"))
 source(findFile("scripts", "peakfit_shared.py"))
+source(findFile("scripts", "dawn_global_ui_controls.py"))
 
 def main():
     #Start using clean workspace
@@ -10,7 +11,8 @@ def main():
     openPerspective("Data Browsing (default)")
     
     #Create space so the drop-down menus show all items
-    createPeakFitSpace(steps=25)
+    if(not isEclipse4()):
+        createPeakFitSpace(steps=25)
     snooze(1)
     
     #expand data tree and open metal mix
@@ -19,7 +21,10 @@ def main():
     mouseClick(waitForObject(":Plot data as separate plots_ToolItem"), 18, 11, 0, Button.Button1)
     
     for i in range(16):
-        mouseClick(waitForObjectItem(":Data_Table", str(i) + "/0"), 9, 7, 0, Button.Button1)
+        if(isEclipse4()):
+            mouseClick(waitForObjectItem(":Data_Table_2", str(i) + "/0"), 9, 7, 0, Button.Button1)
+        else:
+            mouseClick(waitForObjectItem(":Data_Table", str(i) + "/0"), 9, 7, 0, Button.Button1)
     
     snooze(1)
     
