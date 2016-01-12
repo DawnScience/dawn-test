ccd2e828d335a623190038709e2bc71ea1ae330c
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2016-01-12 17:46:58 +0000

    Fix suite use case general for e4

--- org.dawnsci.squishtests/suite_usecases_general/tst_i16_link/test.py
+++ org.dawnsci.squishtests/suite_usecases_general/tst_i16_link/test.py
@@ -15,10 +15,10 @@ def main():
     
     openExternalFile("315029.dat")
     snooze(1) 
-    mouseClick(waitForObjectItem(":Data_Table", "10/0"), 8, 9, 0, Button.Button1)
+    mouseClick(waitForObjectItem(":Data_Table_6", "10/0"), 8, 9, 0, Button.Button1)
     snooze(3) 
-    mouseClick(waitForObjectItem(":Data_Table_4", "0/2"), 19, 27, 0, Button.Button1)
-    mouseDrag(waitForObject(":Data_Scale_2"), 16, 24, 70, 2, Modifier.None, Button.Button1)
+    mouseClick(waitForObjectItem(":Data_Table_7", "0/2"), 19, 27, 0, Button.Button1)
+    mouseDrag(waitForObject(":Data_Scale_3"), 16, 24, 70, 2, Modifier.None, Button.Button1)
     
     system = getPlottingSystem("315029.dat")
     # Check 2D plotted
@@ -27,12 +27,11 @@ def main():
      
     clickTab(waitForObject(":Data_CTabItem"), 25, 10, 0, Button.Button1)
     mouseClick(waitForObject(":Slice as line plots_ToolItem_4"), 17, 14, 0, Button.Button1)
-    mouseClick(waitForObjectItem(":Data_Table_4", "1/2"), 30, 36, 0, Button.Button1)
-    mouseDrag(waitForObject(":Data_Scale_2"), 19, 22, 45, 1, Modifier.None, Button.Button1)
+    mouseClick(waitForObjectItem(":Data_Table_7", "1/2"), 30, 36, 0, Button.Button1)
+    mouseDrag(waitForObject(":Data_Scale_3"), 19, 22, 45, 1, Modifier.None, Button.Button1)
     # Check 1D plotted
     snooze(2)
     test.verify(system.getTraces().iterator().next().getData().getRank() == 1, "Check XY plot")
-
 
     # Exit (or disconnect) DAWN
 
