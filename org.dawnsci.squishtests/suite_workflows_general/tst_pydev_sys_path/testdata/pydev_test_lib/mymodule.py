6d34a164604b36b5a3882afaa3894b202ad34fa7
Baha El Kassaby <baha.elkassaby@gmail.com>
2016-06-23 12:03:02 +0100

    Remove workflow tests

--- org.dawnsci.squishtests/suite_workflows_general/tst_pydev_sys_path/testdata/pydev_test_lib/mymodule.py
+++
@@ -1,4 +0,0 @@
-# the module name and function needs to match
-# but the return value must be different
-def modfunc():
-    return 3
--- org.dawnsci.squishtests/suite_workflows_general/tst_pydev_sys_path/testdata/pydev_test_project/src5lib/myothermodule.py
+++
@@ -1,3 +0,0 @@
-# this module has a different name so it does not shadow mymodule elsewhere
-def modfunc():
-    return 5
